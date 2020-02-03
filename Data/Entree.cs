8fd110c884b9439bf925271715bb54e512011263
Cole Griem <colegriem@gmail.com>
2020-02-03 16:48:53 -0600

    Chili cheese fries

---
+++ Data/Entree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public abstract class Entree
+    {
+        //public abstract double Price { get; set; }
+        //public abstract uint Calories { get; set; }
+        //public List<string> SpecialInstructions;
+    }
+}
